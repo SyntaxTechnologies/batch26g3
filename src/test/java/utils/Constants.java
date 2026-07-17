29bffc2463e9025c1451b5a806d3607d3ac1dfb7
binduj3 <binduj3@gmail.com>
2026-07-17 16:38:11 -0500

    updates to make jenkin build

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -1,7 +1,7 @@
 package utils;
 
 public class Constants {
-    public static final String Config_File_Path=System.getProperty("user.dir")+"/src/test/resources/Config/Config.properties";
+    public static final String Config_File_Path=System.getProperty("user.dir")+"/src/test/resources/config/config.properties";
     public static final int Explicit_Wait=20;
     public static final String SCREENSHOT_FILEPATH = System.getProperty("user.dir")+"/target/screenshots/";
 }
