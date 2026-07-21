939868ffb9cd37873ac443f1712c3eaa9526cda7
binduj3 <binduj3@gmail.com>
2026-07-21 12:36:51 -0500

    Merge from branch 'Mustafa'

--- src/test/java/runners/Testrunner.java
+++ src/test/java/runners/Testrunner.java
@@ -4,7 +4,6 @@ package runners;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.remote.tracing.Tags;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
